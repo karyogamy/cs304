1d4e8cf484c9f95c506cc29cbe688cafaeb5e375
krxsky <brendaxiong@yahoo.ca>
2013-11-05 16:48:27 -0800

    drop can be done in one go now

--- drop.sql
+++ drop.sql
@@ -1,16 +1,25 @@
 -- Author: Steamly Steamy Steamers
 -- reset the state by dropping all tables
 -- the following code generate code to drop all tables:
--- select 'drop table ' || table_name || ';' from user_table;  *
+-- @  *
 -- * http://stackoverflow.com/questions/18030140/drop-all-tables-sql-developer
-drop table PLAYER;
-drop table COMPANY;
-drop table GAME;
-drop table GIFTCARD;
-drop table HAS_ACHIEVEMENT;
-drop table REDEEM;
-drop table SAVE_STORE;
 drop table FRIENDS;
+drop table FAVORITE;
+drop table SAVE_STORE;
 drop table WANTS;
 drop table RANKS;
-drop table FAVORITE;
+drop table REDEEM;
+drop table GIFTCARD;
+drop table BUYS_GAME;
+drop table EARNS;
+drop table HAS_ACHIEVEMENT;
+drop table GAME;
+drop table PLAYER;
+drop table COMPANY;
+
+
+
+
+
+
+
